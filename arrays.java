237e17800de4cbf9be96d97f3b47081ec9df358b
apcs <apcs@MVHS-yearbookimage-5.local>
2013-01-24 08:32:57 -0700

    Initial Commit

---
+++ arrays.java
@@ -0,0 +1,24 @@
+public class arrays {
+	
+	public static void main(String[] args) {
+		int[] nums = {3,6,4,3,2,1,5,6};
+
+		System.out.println(avg(nums));
+	}
+
+
+	public static double avg(int[] nums) {
+		
+		int sum = 0;
+		for (int i = 0;i<nums.length;i++) {
+			sum += nums[i];
+
+		
+		}
+	
+		return ((double) sum) / nums.length;
+
+	}
+
+
+}
